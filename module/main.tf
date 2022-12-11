3505730c82ef90f8b623bf5ea7132303e63a8026
andrewCluey <andrew@andrews-MacBook-Air.local>
2022-12-11 12:35:21 +0000

    added providers

--- module/main.tf
+++ module/main.tf
@@ -1,8 +1,21 @@
-locals {
-  environment = var.environment
-
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.35.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
+
+
+
+locals {
+  environment = var.environment
+}
 
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.environment}-tfcloud"
