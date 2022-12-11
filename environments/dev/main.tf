6a2eb809e640a9f21660af5969b67a41fee0835e
andrewCluey <andrew@andrews-MacBook-Air.local>
2022-12-11 12:36:35 +0000

    added providers

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.35.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 module "dev" {
     source = "../../module"
 
